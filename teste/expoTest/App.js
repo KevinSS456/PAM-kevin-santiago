da573f345f3e9f1186fdf0b8c54eedcc8ca4aa45
KevinSS456 <kesantiago246@gmail.com>
2023-06-23 00:17:38 -0300

    Tarefa pm

--- teste/expoTest/App.js
+++ teste/expoTest/App.js
@@ -1,37 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View,Image, TouchableOpacity } from 'react-native';
+
+import React,{useState, useEffect} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View,Image, TouchableOpacity, Animated } from 'react-native';
 
 export default function App() {
+    const [offset] = useState(new Animated.ValueXY({x: 0, y: 90}));
+    
+
+    useEffect(()=> {
+      Animated.spring(offset.y,{
+        toValue:0,
+        speed: 4,
+        bounciness: 30
+      }).start();
+    },[])
   return (
   <KeyboardAvoidingView style={styles.background}> 
-    <View>
+    <View style={styles.containerLogo}>
       <Image
-        source={'./src/assets/logo.png'}
+        source={require('./assets/logo.png')}
+        style={{ alignSelf: "center" }}
       />
     </View>
     
-    <View>
+    <Animated.View 
+    style={[
+      styles.container,
+      {
+        transform:[
+          { translateY: offset.y}
+        ]
+      }
+    ]}
+    >
       <TextInput
+        style={styles.input}
         placeholder='Email'
         autoCorrect={false}
         onChangeText={()=>{}}
       />
     
       <TextInput
+        style={styles.input}
         placeholder='Senha'
         autoCorrect={false}
         onChangeText={()=>{}}
       />
 
-      <TouchableOpacity>
-        <Text>Acessar</Text>
+      <TouchableOpacity style={styles.btnSubmit}>
+        <Text style={styles.submitText}>Acessar</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Text>Criar conta gratuita</Text>
+      <TouchableOpacity style={styles.btnRegister}>
+        <Text style={styles.submitRegister}>Criar conta gratuita</Text>
       </TouchableOpacity>
 
-    </View>
+    </Animated.View>
   </KeyboardAvoidingView>  
     );
 }
@@ -47,5 +70,39 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: 'white',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10
+  },
+  btnSubmit:{
+    backgroundColor: "#35AAFF",
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
+  submitText:{
+    color: '#FFF',
+    fontSize:18
+  },
+  btnRegister:{
+    marginTop: 10
+  },
+  registerText:{
+    color: '#FFF'
   }
 });
