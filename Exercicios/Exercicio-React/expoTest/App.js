1c3d83a17bd614aa690dda6f4cf8f95d14130b10
KevinSS456 <kesantiago246@gmail.com>
2023-06-23 23:44:16 -0300

    lição de react native

--- Exercicios/Exercicio-React/expoTest/App.js
+++ Exercicios/Exercicio-React/expoTest/App.js
@@ -1,31 +1,45 @@
+//Feito por Isabelli Santiago e Kevin Santiago
+
 
 import React,{useState, useEffect} from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View,Image, TouchableOpacity, Animated } from 'react-native';
 
 export default function App() {
     const [offset] = useState(new Animated.ValueXY({x: 0, y: 90}));
-    
+    const [opacity] = useState(new Animated.Value(0));
 
     useEffect(()=> {
-      Animated.spring(offset.y,{
+      Animated.parallel([
+        Animated.spring(offset.y,{
         toValue:0,
         speed: 4,
         bounciness: 30
-      }).start();
+      }),
+      Animated.timing(opacity,{
+        toValue: 1,
+        duration: 500,      }
+
+      )
+    ]).start()
+      
     },[])
   return (
   <KeyboardAvoidingView style={styles.background}> 
+    
     <View style={styles.containerLogo}>
       <Image
         source={require('./assets/logo.png')}
-        style={{ alignSelf: "center" }}
+        style={{ alignSelf: "center",
+        width: 108,
+        height: 129,  
+      }}
       />
     </View>
-    
     <Animated.View 
     style={[
       styles.container,
       {
+        opacity: opacity,
         transform:[
           { translateY: offset.y}
         ]
